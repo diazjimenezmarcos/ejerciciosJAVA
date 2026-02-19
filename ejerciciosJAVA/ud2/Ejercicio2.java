4cddcdee3756f867bae64185ffe56b1d94776e25
diazjimenezmarcos <diaz.jimenez.marcos@iescamas.es>
2026-02-19 18:04:07 +0100

    Add files via upload

---
+++ ejerciciosJAVA/ud2/Ejercicio2.java
@@ -0,0 +1,20 @@
+package Act2;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime un número: ");
+		int num1 = sc.nextInt();
+		System.out.println("Dime otro número: ");
+		int num2 = sc.nextInt();
+		
+		if (num1 == num2) {
+			System.out.println("Los dos numeros son iguales");
+		}else {
+			System.out.println("Los dos numeros son distintos");
+		}
+	}
+
+}
